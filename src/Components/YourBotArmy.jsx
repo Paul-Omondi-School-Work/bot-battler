09159d609ce673b17304267c8b6a45a822489f05
+ <+>
2023-01-23 19:56:16 +0300

    Feat: Remove buttons from YourBotArmy Reverted back to when the card is clicked it a soldier is released from my squad

--- src/Components/YourBotArmy.jsx
+++ src/Components/YourBotArmy.jsx
@@ -21,7 +21,7 @@ created_at,
 updated_at
 } = warrior;
 return (
-<div key={id} className="card col-sm-6 col-md-4 col-lg-3" style={{ width: "20rem" }}>
+<div key={id} onClick={() => removeWarrior(warrior)} className="card col-sm-6 col-md-4 col-lg-3" style={{ width: "20rem" }}>
 <img src={avatar_url} className="card-img-top" alt="..." />
 <div className="card-body">
 <h5 className="card-title">{name}</h5>
@@ -32,7 +32,7 @@ return (
 <p className="card-text">DAMAGE: {damage}</p>
 <p className="card-text">CREATED_AT: {created_at}</p>
 <p className="card-text">UPDATED_AT: {updated_at}</p>
-<button className="rounded bg-dark" onClick={() => removeWarrior(warrior)}>Release Vet</button>
+
 </div>
 </div>
 );
